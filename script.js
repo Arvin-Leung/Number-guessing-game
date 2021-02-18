d7fae312f21f4f18799e75025f3d03a5b977008c
ArvinLeung <arvin.leung@ocsbstudent.ca>
2021-02-18 15:13:20 +0000

    Took out the designated answer

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // This let statement grabs a random number from 0-6 and sets it to x
-// let answer = Math.floor(Math.random() * 6)
+let answer = Math.floor(Math.random() * 6)
 
-let answer = 4
 // These eventlistners allow the buttons to call the functions
 document.getElementById('btn').addEventListener('click', checker)
 document.getElementById('reset').addEventListener('click', newnumber)
@@ -11,7 +10,6 @@ function checker () {
   // The line below grab the user's guess and stores it into the y variable
   let guess = document.getElementById('guess').value
   guess = parseInt(guess)
-  console.log(answer)
   // The lines below check if the number is too high or too low
   if (guess > answer) {
     alert('Sorry! Guess was too high!')
